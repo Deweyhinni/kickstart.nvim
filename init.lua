be68b892e6b232712eaa8ff8b23017ee4f5e661b
deweyhinni <deweyhinni@protonmail.com>
2024-04-04 19:25:38 +0000

    added some html and htmx stuff

--- init.lua
+++ init.lua
@@ -554,7 +554,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         rust_analyzer = {
           settings = {
             ['rust_analyzer'] = {
@@ -566,6 +566,10 @@ require('lazy').setup({
           },
         },
         gleam = {},
+        htmx = {
+          filetypes = { 'html', 'htmldjango', 'templ' },
+        },
+        emmet_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
